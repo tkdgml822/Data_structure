1653ac8a1f7f6f3ede05f6d2a75e3acfb7eb517d
한상희 <tkdgml822@gmail.com>
2023-08-02 19:38:26 +0900

    깃 연결 리스트

---
+++ 리스트ADT기반으로_정의된_main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "ArrayList.h"
+
+int main(void) {
+    List list;
+    int data;
+    ListInit(&list);
+
+    // data Insert
+    LInsert(&list, 11);
+    LInsert(&list, 11);
+    LInsert(&list, 22);
+    LInsert(&list, 22);
+    LInsert(&list, 33);
+
+    // data_count print
+    printf("현재 리스트에 저장된 데이터 수 : %d\n", LCount(&list));
+
+    // first_data check
+    if(LFirst(&list, &data)) {
+        printf("%d\n", data);
+
+        while(LNext(&list, &data)) {
+            printf("%d", data);
+        }
+    };
+    printf("\n\n");
+    
+    //quest number "22"
+    if(LFirst(&list, &data)) {
+        if(data == 22) {
+            LRemove(&list);
+        }
+
+        while(LNext(&list, &data)) {
+            if (data == 22) {
+                LRemove(&list);
+            }
+        }
+    }
+
+    // Remove data and print All Data counts
+    printf("현재 데이터 수: %d\n", LCount(&list));
+
+    if(LFirst(&list, &data)) {
+        printf("%d", data);
+
+        while(LNext(&list, &data)) {
+            printf("%d", data);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+    return 0;
+}
