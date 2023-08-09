96f02c63f2d041c1b1cb6fffdb221039bdc2697d
한상희 <tkdgml822@gmail.com>
2023-08-10 06:11:56 +0900

    04-3 문제 풀이

---
+++ Chapter4/04-3/ArrayList.h
@@ -0,0 +1,34 @@
+#include "Point.h"
+#ifndef __ARRAY_LIST_H__
+#define __ARRAY_LIST_H__
+
+#define TRUE	1
+#define FALSE	0
+
+/*** ArrayList�� ���� ****/
+#define LIST_LEN	100
+// int를 LData로 재정의(전) -> Point 구조체로 변경 
+typedef Point * LData;
+
+typedef struct __ArrayList {
+	// Point의 구고체 100개 생성
+	LData arr[LIST_LEN];
+	// 데이터 수
+	int numOfData;
+	// 현재 위치
+	int curPosition;
+} ArrayList;
+
+/*** ArrayList 재 정의 추상화 ****/
+typedef ArrayList List;
+
+void ListInit(List * plist);
+void LInsert(List * plist, LData data);
+
+int LFirst(List * plist, LData * pdata);
+int LNext(List * plist, LData * pdata);
+
+LData LRemove(List * plist);
+int LCount(List * plist);
+
+#endif
